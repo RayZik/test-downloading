82dc840dea0e28cdaf09523cf85d88c95786ece2
Artem Shlagin <artem@shlagin.com>
2023-01-18 19:04:36 +0700

    Add one socket test

--- index.ts
+++ index.ts
@@ -161,77 +161,154 @@ export const downloadAppKeepAlive = ({
   };
 };
 
+export const downloadAppOneSocket = ({
+  downloadUrl,
+  destination,
+  customFileName,
+}: {
+  downloadUrl: string;
+  destination: string;
+  customFileName?: string;
+}) => {
+  console.info(
+    `Start downloading "${downloadUrl}" into ${destination} with maxSockets=1`
+  );
+
+  const downloadHelper = new DownloaderHelper(downloadUrl, destination, {
+    timeout: 30000,
+    resumeOnIncomplete: false,
+    resumeOnIncompleteMaxRetry: 0,
+    removeOnStop: false,
+    removeOnFail: false,
+    fileName: customFileName,
+    httpsRequestOptions: {
+      agent: new https.Agent({
+        maxSockets: 1,
+      }),
+    },
+  });
+
+  downloadHelper.on("stateChanged", (state) => {
+    console.info(`Downloading: stateChanged to`, state);
+  });
+
+  const promise = new Promise<boolean>((resolve, reject) => {
+    downloadHelper.on("end", () => resolve(true));
+    downloadHelper.on("error", (error) => {
+      console.error(`Downloading:Error`, error);
+
+      reject(error);
+    });
+
+    downloadHelper.start().catch((error) => {
+      console.error(`Downloading:StartError`, error);
+
+      reject(error);
+    });
+  });
+
+  return {
+    promise,
+    downloadHelper,
+    downloadUrl,
+    destination,
+    customFileName,
+  };
+};
+
 const workflow = async () => {
+  console.log("Running inside", __dirname);
   const normalFilename = "normal.dmg";
   const rangeFilename = "range.dmg";
   const keepAliveFilename = "keepAlive.dmg";
+  const oneSocketFilename = "oneSocket.dmg";
 
-  [normalFilename, rangeFilename, keepAliveFilename].forEach((filename) => {
-    const filePath = path.join(__dirname, filename);
-    if (fs.existsSync(filePath)) {
-      console.log(`rm ${filename}`);
-      fs.unlinkSync(filePath);
+  [normalFilename, rangeFilename, keepAliveFilename, oneSocketFilename].forEach(
+    (filename) => {
+      const filePath = path.join(__dirname, filename);
+      if (fs.existsSync(filePath)) {
+        console.log(`rm ${filename}`);
+        fs.unlinkSync(filePath);
+      }
     }
-  });
+  );
 
   let downloadUrl =
     "https://app-dist-dev.toonly.com/main/1.7.32/2022-12-29_16-31-25/Toonly-1.7.32-main.dev.dmg";
 
-  const [normalError, normalResult] = await to(
-    downloadAppNormally({
-      downloadUrl: downloadUrl,
-      destination: __dirname,
-      customFileName: normalFilename,
-    }).promise
-  );
-
-  console.log("Download App Normal", {
-    err: normalError,
-    result: normalResult,
-  });
-
-  const normalStats = fs.statSync(path.join(__dirname, normalFilename));
-  console.log("Downloaded normally file size: ", normalStats.size);
-
-  const [rangeError, rangeResult] = await to(
-    downloadAppRange({
-      downloadUrl: downloadUrl,
-      destination: __dirname,
-      customFileName: rangeFilename,
-      startDownloadingFromByte: normalStats.size,
-    }).promise
-  );
-
-  console.log("Download App Range", {
-    err: rangeError,
-    result: rangeResult,
-  });
-
-  const rangeFullPath = path.join(__dirname, rangeFilename);
-  if (fs.existsSync(rangeFullPath)) {
-    const rangeStats = fs.statSync(rangeFullPath);
-    console.log("Downloaded range file size: ", rangeStats.size);
-  } else {
-    console.log(
-      "range file not found, perhaps fully downloaded by normal download?"
+  if (false) {
+    const [normalError, normalResult] = await to(
+      downloadAppNormally({
+        downloadUrl: downloadUrl,
+        destination: __dirname,
+        customFileName: normalFilename,
+      }).promise
     );
+
+    console.log("Download App Normal", {
+      err: normalError,
+      result: normalResult,
+    });
+
+    const normalStats = fs.statSync(path.join(__dirname, normalFilename));
+    console.log("Downloaded normally file size: ", normalStats.size);
+
+    const [rangeError, rangeResult] = await to(
+      downloadAppRange({
+        downloadUrl: downloadUrl,
+        destination: __dirname,
+        customFileName: rangeFilename,
+        startDownloadingFromByte: normalStats.size,
+      }).promise
+    );
+
+    console.log("Download App Range", {
+      err: rangeError,
+      result: rangeResult,
+    });
+
+    const rangeFullPath = path.join(__dirname, rangeFilename);
+    if (fs.existsSync(rangeFullPath)) {
+      const rangeStats = fs.statSync(rangeFullPath);
+      console.log("Downloaded range file size: ", rangeStats.size);
+    } else {
+      console.log(
+        "range file not found, perhaps fully downloaded by normal download?"
+      );
+    }
+
+    const [keepAliveError, keepAliveResult] = await to(
+      downloadAppKeepAlive({
+        downloadUrl: downloadUrl,
+        destination: __dirname,
+        customFileName: keepAliveFilename,
+      }).promise
+    );
+
+    console.log("Download App KeepAlive", {
+      err: keepAliveError,
+      result: keepAliveResult,
+    });
+
+    const keepAliveStats = fs.statSync(path.join(__dirname, keepAliveFilename));
+    console.log("Downloaded keepAlive file size: ", keepAliveStats.size);
   }
 
-  const [keepAliveError, keepAliveResult] = await to(
-    downloadAppKeepAlive({
+  const [oneSocketError, oneSocketResult] = await to(
+    downloadAppOneSocket({
       downloadUrl: downloadUrl,
       destination: __dirname,
-      customFileName: keepAliveFilename,
+      customFileName: oneSocketFilename,
     }).promise
   );
 
   console.log("Download App KeepAlive", {
-    err: keepAliveError,
-    result: keepAliveResult,
+    err: oneSocketError,
+    result: oneSocketResult,
   });
 
-  const keepAliveStats = fs.statSync(path.join(__dirname, keepAliveFilename));
-  console.log("Downloaded keepAlive file size: ", keepAliveStats.size);
+  const oneSocketStats = fs.statSync(path.join(__dirname, oneSocketFilename));
+  console.log("Downloaded oneSocket file size: ", oneSocketStats.size);
 };
 
 workflow().then(() => {
